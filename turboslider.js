06a87d3205b6a06a58bf8710d90c4bfb6f0ec4e5
Ville Viklund <turbolihis@gmail.com>
2013-11-05 17:53:27 +0200

    Refactored slide effect

--- turboslider.js
+++ turboslider.js
@@ -4,60 +4,59 @@
  */
 (function ($) {
   $.fn.turboslider = function (option, settings) {
-    var _settings, 
+    var mergedSettings,
       plugin,
       $elem;
 
     return this.each(function () {
-      _settings = $.extend({}, $.fn.turboslider.defaultSettings, settings || {});
+      mergedSettings = $.extend({}, $.fn.turboslider.defaultSettings, settings || {});
       $elem = $(this);
-      plugin = new Turboslider(_settings, $elem);
+      plugin = new Turboslider(mergedSettings, $elem);
       plugin.init();
     });
   };
 
   $.fn.turboslider.defaultSettings = {
-    interval: 2000
+    interval: 2000,
+    animationSpeed: 400
   };
 
   function Turboslider(settings, $elem) {
     this.settings = settings;
     this.$elem = $elem;
-    this.slides = [];
+    this.slideContainer = this.$elem.find('.turboslider-images');
+    this.slideWidth = 0;
 
     return this;
   }
 
   Turboslider.prototype.init = function () {
-    var self = this, 
-      slide;
+    var self = this,
+      slides = this.slideContainer.children(),
+      slide,
+      containerWidth = 1;
 
-    this.$elem.addClass("turboslider-container");
-    this.$elem.children().each(function (idx, el) {
+    slides.each(function (idx, el) {
       slide = $(el);
       slide.addClass("turboslider-slide");
-      self.slides.push(slide);
+      containerWidth += slide.width();
     });
 
-    this.swapImage();
+    this.slideWidth = parseInt(containerWidth / slides.length, 10);
+    this.totalWidth = containerWidth;
+    this.slideContainer.width(containerWidth);
 
-    return this;
+    setInterval(function () {
+      self.slide();
+    }, this.settings.interval);
   };
 
-  Turboslider.prototype.swapImage = function () {
-    var self = this,
-      currentSlide = this.slides.shift(),
-      nextSlide = this.slides[0];
+  Turboslider.prototype.slide = function () {
+    var currentMargin = parseInt(this.slideContainer.css('marginLeft'), 10),
+      margin = this.slideWidth - currentMargin;
 
-    nextSlide.show();
-    currentSlide.hide();
-
-    this.slides.push(currentSlide);
-
-    setTimeout(function () { 
-      self.swapImage(); 
-    }, this.settings.interval);
-
-    return this;
+    this.slideContainer.animate({
+      'marginLeft': ((margin + this.slideWidth) >= this.totalWidth) ? 0 : "-" + margin + "px"
+    }, this.settings.animationSpeed, "swing");
   };
 })(jQuery);
